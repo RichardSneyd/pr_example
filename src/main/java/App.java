0fb8c4fd8d124e131053ae1027c6872f8068bbb9
Richard Sneyd <richardsneyd@hotmail.com>
2024-06-06 12:21:32 +0630

    add to line 6

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Added this on line 4");
+
+        System.out.println("Added this to line 6 on feature_b branch");
     }
 }
